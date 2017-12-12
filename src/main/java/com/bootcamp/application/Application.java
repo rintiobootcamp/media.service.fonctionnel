06e788a9be1591652707bd64f12eac2889972765
mosouley <mo.souley@gmail.com>
2017-12-12 21:42:48 +0100

    fixing the conflicts and add of diskstorage methode

--- src/main/java/com/bootcamp/application/Application.java
+++ src/main/java/com/bootcamp/application/Application.java
@@ -31,7 +31,7 @@ import java.util.Locale;
 @Configuration
 @ComponentScan({"com.bootcamp"})
 @PropertySources({
-        @PropertySource(value = "classpath:/media.sf.properties", ignoreResourceNotFound = true),
+        @PropertySource(value = "classpath:/application.properties", ignoreResourceNotFound = true),
 })
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, FlywayAutoConfiguration.class})
 @EnableCaching
